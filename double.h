df31579d83da0b64476b6aeadad0b4ea86e78971
tinyynoob <sadpotato1920@gmail.com>
2022-05-23 17:23:23 +0800

    Write the multiplication for normal numbers

--- double.h
+++ double.h
@@ -6,7 +6,7 @@ union ieee_double_precision {
     double represent;
 };
 
-void show_bits(DOUBLE x)
+static void show_bits(DOUBLE x)
 {
     for (int i = 63; i >= 0; i--) {
         printf("%u", (unsigned) (x.bits >> i & 1));
@@ -16,17 +16,17 @@ void show_bits(DOUBLE x)
     putchar('\n');
 }
 
-uint64_t get_sign(DOUBLE x)
+static inline uint64_t get_sign(DOUBLE x)
 {
     return x.bits >> 63;
 }
 
-uint64_t get_expn(DOUBLE x)
+static inline uint64_t get_expn(DOUBLE x)
 {
     return (x.bits >> 52) & (uint64_t) 0x7FF;
 }
 
-uint64_t get_mtsa(DOUBLE x)
+static inline uint64_t get_mtsa(DOUBLE x)
 {
     return x.bits & (((uint64_t) 1 << 52) - 1);
 }
--- mult.c
+++ mult.c
@@ -3,17 +3,34 @@
 #include <limits.h>
 #include "double.h"
 
+#define DEBUG 1
+
 DOUBLE mult(DOUBLE x, DOUBLE y)
 {
     DOUBLE ans = {.bits = 0};
-    /* sign */
-    ans.bits |= ((get_sign(x) ^ get_sign(y)) << 63);
-    /* exponents */
-    ans.bits |= ((get_expn(x) + get_expn(y) - 1023) & (uint64_t) 0x7FF) << 52;
-    /* mantissa */
-
+    uint64_t sign = 0, expn = 0, mtsa = 0;
+    sign = get_sign(x) ^ get_sign(y);
+    expn = (get_expn(x) + get_expn(y) - 1023) & (uint64_t) 0x7FF;
+    __uint128_t mprod = ((__uint128_t) 1 << 52 | get_mtsa(x)) * ((__uint128_t) 1 << 52 | get_mtsa(y));
+    int carry = mprod >> (52 + 52 + 1); // \in \{0, 1\}
+    expn += carry;
+    mtsa = (mprod >> (52 - 1 + carry));
+    mtsa = (mtsa >> 1) + (mtsa & 1);    //rounding to nearest
+    mtsa &= ((uint64_t) 1 << 52) - 1;
+    ans.bits |= sign << 63 | expn << 52 | mtsa;
     return ans;
 }
+
+#if DEBUG
+static void test(DOUBLE x, DOUBLE y)
+{
+    printf("x = %lf, y = %lf\n", x.represent, y.represent);
+    show_bits(x);
+    show_bits(y);
+    show_bits((DOUBLE) {.represent = x.represent * y.represent});
+    show_bits(mult(x, y));
+}
+#endif
 
 int main()
 {
@@ -21,12 +38,15 @@ int main()
     // show_bits((DOUBLE) {.bits = 65537});
     // show_bits((DOUBLE) {.bits = UINT64_MAX});
 
-    x.represent = 3.5;
-    show_bits(x);
+    x.represent = 312345;
     y.represent = 7.1;
-    show_bits(y);
-    show_bits((DOUBLE) {.represent = x.represent * y.represent});
-    show_bits(mult(x, y));
+    test(x, y);
+    x.represent = -312345;
+    y.represent = 7.1;
+    test(x, y);
+    x.represent = -312345;
+    y.represent = -7.1;
+    test(x, y);
 
     return 0;
 }
