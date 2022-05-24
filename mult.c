83f3afad7bb98c6b56fbf460711b71ddad0c3be2
tinyynoob <sadpotato1920@gmail.com>
2022-05-24 21:37:50 +0800

    Consider -0 as well in mult.c

--- mult.c
+++ mult.c
@@ -6,7 +6,7 @@
 
 DOUBLE mult(DOUBLE x, DOUBLE y)
 {
-    /* NaN check */
+    /* if NaN involved */
     if ((get_expn(y) == 0x7FFu && get_mtsa(y))) {
         return y;
     } else if ((get_expn(x) == 0x7FFu && get_mtsa(x))) {
@@ -15,15 +15,16 @@ DOUBLE mult(DOUBLE x, DOUBLE y)
 
     uint64_t sign = 0, expn = 0, mtsa = 0;
     sign = get_sign(x) ^ get_sign(y);
-    if (!x.bits && get_expn(y) == 0x7FFu && !get_mtsa(y)) {
+    /* if \pm 0 involved */
+    if (!(x.bits << 1) && get_expn(y) == 0x7FFu && !get_mtsa(y)) {
         expn = 0x7FFu;
         mtsa = 1;
         goto ret;
-    } else if (!y.bits && get_expn(x) == 0x7FFu && !get_mtsa(x)) {
+    } else if (!(y.bits << 1) && get_expn(x) == 0x7FFu && !get_mtsa(x)) {
         expn = 0x7FFu;
         mtsa = 1;
         goto ret;
-    } else if (!x.bits || !y.bits) {
+    } else if (!(x.bits << 1) || !(y.bits << 1)) {
         expn = 0;
         mtsa = 0;
         goto ret;
@@ -31,22 +32,26 @@ DOUBLE mult(DOUBLE x, DOUBLE y)
 
     expn = (get_expn(x) + get_expn(y) - 1023);
     __uint128_t mprod = ((__uint128_t) 1 << 52 | get_mtsa(x)) * ((__uint128_t) 1 << 52 | get_mtsa(y));
-    int carry = mprod >> (52 + 52 + 1); // \in \{0, 1\}
+    int carry = mprod >> (52 + 52 + 1); // \in {0, 1}
     expn += carry;
     // show_bits((DOUBLE) {.bits = expn});
-    if ((int64_t) expn >= 0x7FFu) { // magnitude too big
+    if ((int64_t) expn >= 0x7FFu) { // magnitude too big, rounding up to \infty
         expn = 0x7FFu;
         mtsa = 0;
         goto ret;
-    } else if ((int64_t) expn <= 0) {   // magnitude too small
+    }
+    mtsa = (mprod >> (52 - 1 + carry));
+    mtsa = (mtsa >> 1) + (mtsa & 1);    //rounding to nearest
+
+
+    mtsa &= ((uint64_t) 1 << 52) - 1;
+
+    expn &= (uint64_t) 0x7FFu;
+    if ((int64_t) expn <= 0) {   // magnitude too small
         expn = 0;
         mtsa = 0;
         goto ret;
     }
-    expn &= (uint64_t) 0x7FFu;
-    mtsa = (mprod >> (52 - 1 + carry));
-    mtsa = (mtsa >> 1) + (mtsa & 1);    //rounding to nearest
-    mtsa &= ((uint64_t) 1 << 52) - 1;
 ret:;
     return (DOUBLE) {.bits = sign << 63 | expn << 52 | mtsa};
 }
