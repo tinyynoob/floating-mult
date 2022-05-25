89bf4cb26e425fc7b3de27859bcfcaa50400abb5
tinyynoob <sadpotato1920@gmail.com>
2022-05-26 01:05:10 +0800

    Add subnormal flag and revise prod in fp_mult.v
    A and B may be swapped to ensure the only subnormal number at B.
    
    At cycle 1, add the two-subnormal case to checking list.
    
    Since A and B may be swapped at cycle 1, we delay the multiplcation
    operations 1 cycle.

--- fp_mult.v
+++ fp_mult.v
@@ -24,22 +24,27 @@ module fp_mult(CLK, RESET, ENABLE, DATA_IN, DATA_OUT, READY);
             inend <= 1;
     end
         
-    reg [63:0] A;
+    reg [63:0] A, B;
+    reg subnormal;  // if there is subnormal number
     always @(posedge CLK) begin
         // no need to reset
         if (ENABLE && !incount[3]) begin
             A <= A << 8;
             A[7:0] <= DATA_IN;
         end
+        else if (inend && !subnormal && !A[62:52] && A[51:0])
+        // subnormal number would be swapped to B if there is
+            A <= B;
     end
 
-    reg [63:0] B;
     always @(posedge CLK) begin
         // no need to reset
         if (ENABLE && incount[3]) begin
             B <= B << 8;
             B[7:0] <= DATA_IN;
         end
+        else if (inend && !subnormal && !A[62:52] && A[51:0])
+            B <= A;
     end
 
     reg [2:0] calcount; // at calculation stage
@@ -50,6 +55,14 @@ module fp_mult(CLK, RESET, ENABLE, DATA_IN, DATA_OUT, READY);
             calcount <= calcount + 1;
     end
 
+    always @(posedge CLK) begin
+        if (RESET)
+            subnormal <= 0;
+        else if (inend && calcount == 0 && 
+                    ((!A[62:52] && A[51:0]) || (!B[62:52] && B[51:0])))
+            subnormal <= 1;
+    end
+
     reg calend;
     reg [63:0] Z;
     always @(posedge CLK) begin
@@ -58,59 +71,63 @@ module fp_mult(CLK, RESET, ENABLE, DATA_IN, DATA_OUT, READY);
             calend <= 0;
         end
         else if (inend && calcount == 0) begin
-            if (A[62:52] == ~11'd0 && A[51:0]) begin
+            // A is NaN
+            if (A[62:52] == {11{1'b1}} && A[51:0]) begin
                 Z <= A;
                 calend <= 1;
             end
-            else if (B[62:52] == ~11'd0 && B[51:0]) begin
+            // B is NaN
+            else if (B[62:52] == {11{1'b1}} && B[51:0]) begin
                 Z <= B;
                 calend <= 1;
             end
-            else if (!A && B[62:52] == ~11'd0 && !B[51:0]) begin
+            // A is 0 and B is \infty
+            else if (!A[62:0] && B[62:52] == {11{1'b1}} && !B[51:0]) begin
                 Z[63] <= A[63] ^ B[63];
-                Z[62:52] <= ~11'd0;
-                Z[51:0] <= 52'd1;
+                Z[62:52] <= {11{1'b1}};
+                Z[0] <= 1;
                 calend <= 1;
             end
-            else if (!B && A[62:52] == ~11'd0 && !A[51:0]) begin
+            // B is 0 and A is \infty
+            else if (!B[62:0] && A[62:52] == {11{1'b1}} && !A[51:0]) begin
                 Z[63] <= A[63] ^ B[63];
-                Z[62:52] <= ~11'd0;
-                Z[51:0] <= 52'd1;
+                Z[62:52] <= {11{1'b1}};
+                Z[0] <= 1;
                 calend <= 1;
             end
-            else if (!A || !B) begin
+            // A or B is 0 and both not \infty
+            else if (!A[62:0] || !B[62:0]) begin
                 Z[63] <= A[63] ^ B[63];
                 calend <= 1;
-                // Z[62:0] <= 63'd0;    originally 0
+                // Z[62:0] <= 0;    originally 0
+            end
+            // A and B are subnormal numbers
+            else if (!A[62:52] && A[51:0] && !B[62:52] && B[51:0]) begin
+                Z[63] <= A[63] ^ B[63];
+                calend <= 1;
+                // Z[62:0] <= 0;
             end
         end
-        else if (!calend && calcount == 3) begin
-            Z[12:0] <= A[62:52] + B[62:52]; // temporarily use the space
-        end
-        else if (!calend && calcount == 4) begin
-            Z[12:0] <= Z[12:0] + prod[103];
-        end
-        // -1023
         // still much to do
         // ...
     end
     
-    reg [103:0] prod;   // 104 = 52 + 52
+    reg [105:0] mprod;   // 106 = 2 * (52 + 1)
     // cut the multiplication to 4 clock-cycles
     always @(posedge CLK) begin
-        if (RESET)
-            prod <= 0;
-        else if (inend && calcount == 0) begin
-            prod <= A * B[12:0];
-        end
-        else if (!calend && calcount == 1) begin
-            prod <= prod + A * {B[25:13], 13'd0};
+        // no need to reset
+        // starting multiplication after possibly swap
+        if (!calend && calcount == 1) begin
+            mprod <= A * B[13:0];
         end
         else if (!calend && calcount == 2) begin
-            prod <= prod + A * {B[38:26], 26'd0};
+            mprod <= mprod + A * {B[26:14], 14'd0};
         end
         else if (!calend && calcount == 3) begin
-            prod <= prod + A * {B[51:39], 39'd0};
+            mprod <= mprod + A * {B[39:27], 27'd0};
+        end
+        else if (!calend && calcount == 4) begin
+            mprod <= mprod + A * {~subnormal, B[51:40], 40'd0};
         end
     end
 
